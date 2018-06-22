061a8d916c041a71cb33c19b43def029db0a0d58
RAYHAN01 <rayrichardhan@hotmail.com>
2018-06-21 19:04:59 -0700

    changed rec condition threshold to 250

--- hdwr/PCB2001/PCB2001.ino
+++ hdwr/PCB2001/PCB2001.ino
@@ -29,22 +29,22 @@
 
 int base_threshold_1 = 80; //experimental value (variable)
 int inc_threshold_1 = 65; //experimental value (variable)
-int rec_threshold_1 = 170; //experimental value (variable)
+int rec_threshold_1 = 250; //experimental value (variable)
 bool if_channel_1_inc = false;
 
 int base_threshold_2 = 80; //experimental value (variable)
 int inc_threshold_2 = 65; //experimental value (variable)
-int rec_threshold_2 = 170; //experimental value (variable)
+int rec_threshold_2 = 250; //experimental value (variable)
 bool if_channel_2_inc = false;
 
 int base_threshold_3 = 80; //experimental value (variable)
 int inc_threshold_3 = 65; //experimental value (variable)
-int rec_threshold_3 = 170; //experimental value (variable)
+int rec_threshold_3 = 250; //experimental value (variable)
 bool if_channel_3_inc = false;
 
 int base_threshold_4 = 80; //experimental value (variable)
 int inc_threshold_4 = 65; //experimental value (variable)
-int rec_threshold_4 = 170; //experimental value (variable)
+int rec_threshold_4 = 250; //experimental value (variable)
 bool if_channel_4_inc = false;
 
 char ssid[] = "NETGEAR40";      //  your network SSID (name)
